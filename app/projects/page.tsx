3abf6f1c1452cc56c0cc1795c58422d3103ef043
Lars-Ostervold <ostervold.berent@gmail.com>
2024-02-02 10:25:29 -0600

    Update projects w/ links

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,8 @@
 import { MiniProjectCard } from '../components/MiniProjectCard'
+import { ProjectCard } from '../components/ProjectCard'
 
 async function getRepoData() {
-  const res = await fetch('https://api.github.com/users/lars-ostervold/repos' , {
+  const res = await fetch(`https://api.github.com/users/lars-ostervold/repos?${Date.now()}`, {
     headers: {
       'Cache-Control': 'no-cache'
     }
@@ -18,15 +19,31 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className="font-medium text-3xl mb-4 tracking-tighter">🚀 My Programming Projects 🖥️</h1>
+      <h1 className="font-medium text-3xl mb-4 tracking-tighter">Programming Projects 🖥️</h1>
+      <p className="prose prose-neutral dark:prose-invert mb-8">
+        Here's a collection of some of my programming projects. If there's a public website, clicking
+        the link will take you there. If not, the link will take you to the GitHub repository.
+      </p>
       <div className="projects-grid grid grid-cols-1 gap-4">
         {repoData.map((repo, index) => (
           <MiniProjectCard
             key={index}
             title={repo.name}
             description={repo.description}
+            url={repo.html_url}
+            homepage={repo.homepage}
           />
         ))}
+      </div>
+      <h1 className="font-medium text-3xl mb-4 mt-8 tracking-tighter">What else is cooking... 👨🏼‍🍳</h1>
+      <p className="prose prose-neutral dark:prose-invert mb-8">
+        A collection of other passion projects that aren't programming 😇
+      </p>
+      <div className="projects-grid grid grid-cols-2 gap-4">
+        <ProjectCard title="BREAD!🥖" 
+          description="I've been making a ridiculous amount of sourdough recently because it's rly rly fun." 
+          imageUrl="/images/bread-photo.JPG"
+        />
       </div>
     </main>
   )
