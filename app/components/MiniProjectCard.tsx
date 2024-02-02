403b52ec90ff7895ce4e6eed17c13f94f31c701d
Lars-Ostervold <ostervold.berent@gmail.com>
2024-02-02 08:41:21 -0600

    light mode edits

--- app/components/MiniProjectCard.tsx
+++ app/components/MiniProjectCard.tsx
@@ -9,9 +9,9 @@ interface ProjectCardProps {
 
 export const MiniProjectCard: React.FC<ProjectCardProps> = ({ title, description }) => {
     return (
-      <div className="card bg-gray-900 text-white rounded-lg shadow-md p-4 border border-gray-600 overflow-hidden transform transition-transform duration-200 ease-in-out hover:scale-105">
-        <h2 className="text-2xl font-bold mt-1 mb-4 text-gray-300">{title}</h2>
-        <p className="text-sm text-gray-100">{description}</p>
+      <div className="card bg-white dark:bg-gray-900 text-black dark:text-white rounded-lg shadow-md p-4 border border-gray-400 dark:border-gray-600 overflow-hidden transform transition-transform duration-200 ease-in-out hover:scale-105">
+        <h2 className="text-2xl font-bold mt-1 mb-4 dark:text-gray-300">{title}</h2>
+        <p className="text-sm dark:text-gray-100">{description}</p>
       </div>
     );
   };
