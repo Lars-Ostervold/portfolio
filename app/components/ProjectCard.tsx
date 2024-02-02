91f7bbcd0a3e34c1d5ddfaac7bb337026343bd85
Lars-Ostervold <ostervold.berent@gmail.com>
2024-02-02 11:25:05 -0600

    Update home projects

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,19 +1,31 @@
 // app/components/ProjectCard.tsx
 
 import React from 'react';
+import Tooltip from '@mui/material/Tooltip';
 
 interface ProjectCardProps {
   title: string;
   description: string;
   imageUrl: string;
+  url: string | null;
 }
 
-export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl }) => {
-    return (
-      <div className="card bg-white dark:bg-gray-900 text-black dark:text-white rounded-lg shadow-md p-4 border border-gray-400 dark:border-gray-600 overflow-hidden transform transition-transform duration-200 ease-in-out hover:scale-105">
-        <img src={imageUrl} alt={title} className="w-full object-cover rounded-lg" />
-        <h2 className="text-xl mt-2 dark:text-gray-300">{title}</h2>
-        <p className="text-sm dark:text-gray-100">{description}</p>
-      </div>
-    );
-  };
+export const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, imageUrl, url }) => {
+  const card = (
+    <div className="card bg-white dark:bg-gray-900 text-black dark:text-white rounded-lg shadow-md p-4 border border-gray-400 dark:border-gray-600 overflow-hidden transform transition-transform duration-200 ease-in-out hover:scale-105">
+      <img src={imageUrl} alt={title} className="w-full object-cover rounded-lg" />
+      <h2 className="text-xl mt-2 dark:text-gray-300">{title}</h2>
+      <p className="text-sm dark:text-gray-100">{description}</p>
+    </div>
+  );
+
+  return url ? (
+    <a href={url} target='_blank' rel='noreferrer noopener'>
+      {card}
+    </a>
+  ) : (
+    <Tooltip title="There's no link for this project" placement="right">
+      <div>{card}</div>
+    </Tooltip>
+  );
+  }; 
